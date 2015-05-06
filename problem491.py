340a5e8d647b17b027ab00934c328d3c772a9608
yondaime008 <ben.nasr.atef@outlook.fr>
2015-05-06 17:51:22 +0100

    Added new 11 multiplier method

--- problem491.py
+++ problem491.py
@@ -1,32 +1,31 @@
-from itertools import *
-
-numbers = '01234567890123456789'
-
-
-def generate_double_palindrome(string):
-    result = 0
-    for j in permutations(string, 20):
-        number = ''
-        for i in j:
-            number += str(i)
-        if number[0] != '0' and is_eleven_multiplier(int(number)):
-            result += 1
-    print("THE RESULT IS: ", result)
+import itertools
+import math
 
 
-def is_eleven_multiplier(n):
-    even_sum = 0
-    uneven_sum = 0
-    length = len(str(n))
-    for j in range(0, length):
-        if j % 2 == 0:
-            even_sum += int(str(n)[j])
-        else:
-            uneven_sum += int(str(n)[j])
-    if even_sum - uneven_sum == 0 or (even_sum - uneven_sum) % 11 == 0:
-        return True
-    else:
-        return False
+def count_number(n):
+    for count in itertools.product(range(3), repeat=10):
+        if sum(count) != 10:
+            continue
+        if sum([i * count[i] for i in range(10)]) == n:
+            yield(count)
 
 
-generate_double_palindrome(numbers)
+def valid_odd_sum():
+    for i in range(-5, 6):
+        if i % 2 == 0:
+            yield i*11/2+45
+
+
+somme_odd = 0
+somme_zero = 0
+for i in valid_odd_sum():
+    i_even = 90 - i
+    for j in count_number(i):
+        somme_occ = 1
+        if j[0] != 0:
+            somme_zero += math.factorial(9)*math.factorial(10)
+        for k in j:
+            if k == 2:
+                somme_occ *= k
+        somme_odd += math.factorial(10)/somme_occ
+print(somme_odd**2-somme_zero)
--- test.py
+++ test.py
@@ -1,36 +1,32 @@
-# class Point:
-#    def __init( self, x=0, y=0):
-#       self.x = x
-#       self.y = y
-#    def __del__(self):
-#       class_name = self.__class__.__name__
-#       print(class_name, "destroyed")
+def read_triangle(file):
+    f = open(file, 'r')
+    triangle = {}
+    lines = f.read().splitlines()
+    for line in lines:
 
-# pt1 = Point()
-# pt2 = pt1
-# pt3 = pt1
-# print(id(pt1), id(pt2), id(pt3)) # prints the ids of the obejcts
-# del pt1
-# del pt2
-# del pt3
+        return line
 
+line = read_triangle('cipher.txt')
+somme = 0
+a = 103
+b = 111
+c = 100
+i = 0
+result = ''
+for n in line.split(','):
+    if i % 3 == 0:
+        n = str(int(n) ^ a)
+    elif i % 3 == 1:
+        n = str(int(n) ^ b)
+    else:
+        n = str(int(n) ^ c)
+    i += 1
+    result += chr(int(n))
+if 'the' in result or 'The' in result:
+    for n in result:
+        if n != '':
+            somme += ord(n)
+print(result)
+print(somme)
 
-# class Block:
-#     """Block is one of the many constituants of the castle"""
-#     width = 0
-#     height = 0
-#     index = 0
-#     def __init__(self, width, height, index):
-#         super(Block, self).__init__()
-#         self.width = width
-#         self.height = height
-#         self.index = index
-# b1 = Block(1,10,13)
-# print(id(b1),b1.width,b1.height,b1.index)
-
-import itertools
-from itertools import *
-
-for j in combinations('ABCD', 1):
-    print(j)
 
